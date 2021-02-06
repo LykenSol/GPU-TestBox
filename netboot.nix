b693ce0d770934c16b0fbeadb5636e44a2fe3d1a
Eduard-Mihai Burtescu <eddyb@lyken.rs>
2021-02-07 00:44:29 +0200

    Move to a Headless Vulkan (Nvidia+RADV) configuration.

--- netboot.nix
+++ netboot.nix
@@ -7,25 +7,52 @@ let
         <nixpkgs/nixos/modules/installer/netboot/netboot-base.nix>
       ];
 
+      # HACK(eddyb) `hardware.{amdgpu,nvidia}` only work with `"amdgpu"`/`"nvidia"`
+      # as entries in `services.xserver.videoDrivers`.
+      services.xserver.videoDrivers = [ "amdgpu" "nvidia" ];
+
       nixpkgs.config.allowUnfree = true;
+      hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta.overrideAttrs ({...}: {
+        # HACK(eddyb) `libGLX_nvidia.so` needs X while `libEGL_nvidia.so` is portable.
+        preFixup = ''
+          sed -i 's/libGLX_nvidia/libEGL_nvidia/' {$lib32,$out}/share/vulkan/icd.d/nvidia_icd*.json
+        '';
+      });
 
-      hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
-
-      services.xserver = {
-        enable = true;
-        videoDrivers = [ "nvidia" ];
-        libinput.enable = true;
-        displayManager.sddm = {
-          enable = true;
-          settings = {
-            Autologin = {
-              User = "nixos";
-              Session = "plasma.desktop";
-            };
-          };
-        };
-        desktopManager.plasma5.enable = true;
+      # Headless Vulkan (Nvidia+RADV)
+      boot.kernelParams = [ "radeon.si_support=0" "amdgpu.si_support=1" ];
+      boot.kernelModules = [ "nvidia" "nvidia_modeset" "nvidia_drm" ];
+      services.udev.extraRules = ''
+        # HACK(eddyb) harcoding 0 instead of %n because nvidia GPUs have their own numbering
+        KERNEL=="card*", SUBSYSTEM=="drm", DRIVERS=="nvidia", RUN+="${pkgs.runtimeShell} -c 'mknod -m 666 /dev/nvidia0 c $$(grep nvidia-frontend /proc/devices | cut -d \  -f 1) 0'"
+      '';
+      hardware.nvidia.prime = {
+        offload.enable = true;
+        # HACK(eddyb) these are needed to avoid an assertion but aren't actually
+        # used outside of adding configuration to X (which is disabled anyway).
+        nvidiaBusId = "N/A";
+        intelBusId = "N/A";
       };
+      hardware.opengl = {
+        enable = true;
+        driSupport32Bit = true;
+      };
+
+      # Graphical (X11) configuration.
+      # services.xserver = {
+      #   enable = true;
+      #   libinput.enable = true;
+      #   displayManager.sddm = {
+      #     enable = true;
+      #     settings = {
+      #       Autologin = {
+      #         User = "nixos";
+      #         Session = "plasma.desktop";
+      #       };
+      #     };
+      #   };
+      #   desktopManager.plasma5.enable = true;
+      # };
 
       environment.systemPackages = with pkgs; [
         vulkan-tools
