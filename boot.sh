c5f28afee964e78d27cf89764bc291013d80ac26
Eduard-Mihai Burtescu <eddyb@lyken.rs>
2021-02-07 00:08:48 +0200

    boot.sh: optional WoL (Wake-Up on LAN) and SSH reboot support.

--- boot.sh
+++ boot.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
 # NOTE(eddyb) most of this is copied from https://nixos.wiki/wiki/Netboot
-set -euo pipefail
 
 nix build -f netboot.nix --out-link ./netboot
 
 cmdline=$(grep -o 'init=.*' ./netboot/netboot.ipxe | sed 's/ initrd=initrd//')
+
+if [ -n "$GPU_TEST_BOX_MAC" ]; then
+  wol $GPU_TEST_BOX_MAC
+fi
+
+if [ -n "$GPU_TEST_BOX_IP" ]; then
+  ssh nixos@$GPU_TEST_BOX_IP -o StrictHostKeyChecking=no sudo reboot
+fi
 
 # NOTE(eddyb) required configuration, under `networking.firewall`
 # (or `networking.firewall.interfaces.<specific interface name>`):
